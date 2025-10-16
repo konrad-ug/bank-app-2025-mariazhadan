c49151fc584ec4dee6f09a21de33b72684a61017
Mariia Zhadaniuk <mzhadaniuk@INF.UG.EDU.PL>
2025-10-16 14:02:00 +0200

    zad1 v.1

--- src/account.py
+++ src/account.py
@@ -1,4 +1,17 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name,pesel, promocode = None):
         self.first_name = first_name
         self.last_name = last_name
+        self.balance = 0.0
+        # if pesel is not None and len(pesel) == 11:
+        #     self.pesel = pesel
+        # else: 
+        #     self.pesel = "Invalid"
+        self.pesel = pesel if self.pesel_is_valid(pesel) else "Invalid"
+
+    def pesel_is_valid(self,pesel):
+        if pesel is not None and len(pesel) == 11:
+            return True
+
+
+    
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,6 +3,20 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "12345678901")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.balance == 0.0
+        assert account.pesel == "12345678901"
+
+    def test_pesel_long(self):
+        account = Account("John", "Doe", "123456789000")
+        assert account.pesel == "Invalid"
+
+    def test_pesel_short(self):
+        account = Account("John", "Doe", "12345")
+        assert account.pesel == "Invalid"
+
+    def test_pesel_long(self):
+        account = Account("John", "Doe", None)
+        assert account.pesel == "Invalid"
