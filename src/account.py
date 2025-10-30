5017861b0bf0e6e6bcf615f7533ab72bc2c7d22a
Mariia Zhadaniuk <m.zhadaniuk.670@studms.ug.edu.pl>
2025-10-30 12:09:50 +0100

    Lab1

--- src/account.py
+++ src/account.py
@@ -8,10 +8,54 @@ class Account:
         # else: 
         #     self.pesel = "Invalid"
         self.pesel = pesel if self.pesel_is_valid(pesel) else "Invalid"
+         if 
+            promocode is not None
+            and self.promocode_is_valid(promocode)
+            and self.is_eligible_for_promo(pesel)
+        :
+            self.balance += 50
+        
 
     def pesel_is_valid(self,pesel):
         if pesel is not None and len(pesel) == 11:
             return True
 
+    def promocode_is_valid(self,promocode):
+        if not isinstance(promocode, str):
+            return False
+        if promocode.startswith("PROM_") and len(promocode) > 5:
+            suffix = promocode[5:]  
+            if not suffix.isdigit():  
+                return True
+        return False
+
+    def extract_birth_year(self, pesel):
+        if not self.pesel_is_valid(pesel):
+            return None
+
+        year = int(pesel[0:2])
+        month = int(pesel[2:4])
+        if 1 <= month <= 12:
+            century = 1900
+        elif 21 <= month <= 32:
+            century = 2000
+            month -= 20
+        elif 41 <= month <= 52:
+            century = 2100
+            month -= 40
+        elif 61 <= month <= 72:
+            century = 2200
+            month -= 60
+        elif 81 <= month <= 92:
+            century = 1800
+            month -= 80
+        else:
+            return None  
+
+        return century + year
+
+     def is_eligible_for_promo(self, pesel):
+        year = self.extract_birth_year(pesel)
+        return year is not None and year > 1960
 
     
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -20,3 +20,28 @@ class TestAccount:
     def test_pesel_long(self):
         account = Account("John", "Doe", None)
         assert account.pesel == "Invalid"
+
+    def test_promo_valid(self): 
+        account = Account("John", "Doe", "12345678901","PROM_ABC")
+        assert account.balance == 50.0
+    
+    def test_promo_invalid_prefix(self):
+        account = Account("John", "Doe", "12345678901","PRtM_ABC")
+        assert account.balance == 0.0
+
+    def test_promo_short(self):
+        account = Account("John", "Doe", "12345678901","PRM_C")
+        assert account.balance == 0.0
+    
+    def test_promo_not_str(self):
+        account = Account("John", "Doe", "12345678901",4567)
+        assert account.balance == 0.0
+
+    def test_promo_eligible_after_1960():
+        account = Account("John", "Doe", "70010112345", "PROM_ABC")
+        assert account.balance == 50.0
+    
+    
+    def test_promo_not_eligible_before_1960():
+        account = Account("John", "Doe", "59123112345", "PROM_ABC")
+        assert account.balance == 0.0
